b2178886f1db668e34d79bb9461d6381c6582404
yuanyueshui <yuanyueshui@wepiao.com>
2017-11-08 23:56:39 +0800

    test

--- dep.php
+++ dep.php
@@ -18,12 +18,15 @@
 	);
 	// Run the commands for output
 	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
+	$mima = trim(shell_exec('echo $MIMA'));
+	if($_GET['mima'] == $mima) {
+		foreach($commands AS $command){
+			// Run it
+			$tmp = shell_exec($command);
+			// Output
+			$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+			$output .= htmlentities(trim($tmp)) . "\n";
+		}
 	}
 	// Make it pretty for manual user access (and why not?)
 ?>
