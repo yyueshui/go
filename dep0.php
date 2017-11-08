2e3b9451bf567ee0f951b9ca71a5499e0dce8581
yuanyueshui <yuanyueshui@wepiao.com>
2017-11-08 23:46:13 +0800

    test

---
+++ dep0.php
@@ -0,0 +1,23 @@
+<?php
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
+	 */
+	// The commands
+	$commands = array(
+		'echo $NAME',
+	);
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+	// Make it pretty for manual user access (and why not?)
+?>
